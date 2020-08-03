c9175d6934238b3e17f9116b9aa1ab81d7e8f1f0
river <11021252@qq.com>
2020-08-03 15:57:11 +0800

    new一个新的子弹类，在屏幕中画出来

---
+++ src/main/java/com/mashibing/tank/Bullet.java
@@ -0,0 +1,41 @@
+package com.mashibing.tank;
+
+import java.awt.*;
+
+public class Bullet {
+    private static final int SPEED = 7;
+    private static int WIDTH=30,HEIGHT=30;
+
+    private int x,y;
+    private Dir dir;
+
+    public Bullet(int x, int y, Dir dir) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+    }
+    public void paint(Graphics g){
+        Color c =g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x,y,WIDTH,HEIGHT);
+        g.setColor(c);
+        move();
+    }
+    //根据方向移动子弹
+    private void move(){
+        switch (dir){
+            case LEFT:
+                x-=SPEED;
+                break;
+            case UP:
+                y-=SPEED;
+                break;
+            case RIGHT:
+                x+=SPEED;
+                break;
+            case DOWN:
+                y+=SPEED;
+                break;
+        }
+    }
+}
