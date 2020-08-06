d8baa8f9d306742ecc41a38fce3801c7f68df2e9
river <11021252@qq.com>
2020-08-06 17:42:49 +0800

    1.1版本完结

--- src/main/java/com/mashibing/tank/Main.java
+++ src/main/java/com/mashibing/tank/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) throws Exception{
         TankFrame tf = new TankFrame();
 
+        int initTankCount = PropertyMgr.get("initTankCount");
+
         //初始化敌方坦克
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < initTankCount; i++) {
             tf.tanks.add(new Tank(50+i*80,200,Dir.DOWN,Group.BAD,tf));
         }
         //背景音乐
